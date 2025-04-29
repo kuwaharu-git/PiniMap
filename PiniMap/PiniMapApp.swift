552e470dc7ed77c0ea0ede598bbb4c950652ab11
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-04-29 14:51:51 +0900

    create project

---
+++ PiniMap/PiniMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiniMapApp.swift
+//  PiniMap
+//
+//  Created by kuwaharu on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct PiniMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
